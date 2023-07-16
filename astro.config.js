39cc78a45c50f4b00d5e4dfca0cfd8596166a96f
Alan North <alanscodelog@gmail.com>
2023-07-15 21:02:00 -0300

    deps: updated deps

--- astro.config.js
+++ astro.config.js
@@ -1,35 +1,46 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
-import image from "@astrojs/image";
-
+import mdx from "@astrojs/mdx"
 // https://astro.build/config
-import vue from "@astrojs/vue";
-
+import node from "@astrojs/node"
+import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
-import node from "@astrojs/node";
-
+import svelte from "@astrojs/svelte"
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
+import tailwind from "@astrojs/tailwind"
+import vue from "@astrojs/vue"
+import { defineConfig, sharpImageService } from "astro/config"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	site:"https://alanscodelog.com",
-	integrations: [
-		mdx(),
-		sitemap(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp'
-		}),
-		vue({
-			appEntrypoint: '/src/components/App.ts'
-		}),
-		tailwind()
-	],
-	adapter: node({
-		mode: "standalone"
-	})
-});
+  output: "static",
+  site: "https://alanscodelog.com",
+  integrations: [
+	mdx(),
+	sitemap(),
+	vue({ appEntrypoint: "/src/components/App.ts" }),
+	tailwind({ configFile: "tailwind.config.js" }),
+	svelte(),
+	icon({
+      include: {
+        devicon: ["*"],
+      },
+    }),
+],
+  image: {
+    service: sharpImageService(),
+  },
+  markdown: {
+    shikiConfig: {
+      // theme: 'one-dark-pro',
+      theme: "dark-plus",
+      wrap: true,
+    },
+    smartypants: false,
+  },
+  // adapter: node({
+  // 	mode: "standalone"
+  // }),
+  experimental: {
+    assets: true,
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/* Note, esm tailwind not working. And typescript version works but changes to theme don't get applied. */
+// const plugin = require("tailwindcss/plugin")
+// const {fontFamily} = require('tailwindcss/defaultTheme')
+// const textFillStroke = require('tailwindcss-text-fill-stroke')
+import { fontFamily } from "tailwindcss/defaultTheme"
+import plugin from "tailwindcss/plugin"
+import textFillStroke from "tailwindcss-text-fill-stroke"
+
+/** import("tailwindcss").Config; */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+
+		fontFamily: {
+			// "display": [`"Oxanium Variable"`/* ,...fontFamily.sans */],
+			// 'display': [`"Grenze Gotisch Variable"`,...fontFamily.sans],
+			// 'display': [`"Cinzel Decorative"`,...fontFamily.sans],
+			display: [`"Megrim"`, ...fontFamily.sans],
+			body: [...fontFamily.sans],
+			mono: [`"Hack"`, ...fontFamily.mono],
+			title: [`"Share Tech Mono"`, ...fontFamily.mono],
+		},
+		extend: {
+			fontSize: {
+				xs: "0.75rem",
+			},
+			screens: {
+				"not-print": { raw: "not print" },
+			},
+			"text-shadow": {
+				sm: "0 1px 2px var(--tw-shadow-color)",
+				DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+				md: "0 2px 4px var(--tw-shadow-color)",
+				lg: "0 6px 8px var(--tw-shadow-color)",
+			},
+			gridTemplateColumns: theme => {
+				const spacing = theme("spacing")
+				return {
+					...Object.fromEntries(Object.keys(spacing).map(key =>
+					[`fill-${key}`, `repeat(auto-fill, minmax(${spacing[key]}, 1fr))`],
+					)),
+					...Object.fromEntries(Object.keys(spacing).map(key =>
+						[`fit-${key}`, `repeat(auto-fit, minmax(${spacing[key]}, 1fr))`],
+					)),
+				}
+			},
+		},
+	},
+	plugins: [
+		textFillStroke,
+		plugin(function({ matchUtilities, theme }) {
+			matchUtilities(
+				{ "text-shadow": value => ({ "text-shadow": value }) },
+				{ values: theme("text-shadow") },
+			)
+			matchUtilities(
+				{ "grid-cols-fit": value => ({ "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))` }) },
+			)
+			matchUtilities(
+				{ "grid-cols-fill": value => ({ "grid-template-columns": `repeat(auto-fill, minmax(${value}, 1fr))` }) },
+			)
+		}),
+	],
+}
