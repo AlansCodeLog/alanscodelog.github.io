0819a7fd1900e6282607485995befb0d5a92bede
Alan North <alanscodelog@gmail.com>
2023-07-17 18:46:00 -0300

    feat: SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,16 +2,19 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Alan's Code Log"
-export const SITE_USERNAME = "alan's code log"
-export const SITE_DESCRIPTION = ""
+export const SITE_DESCRIPTION = "My coding blog."
 export const IMAGE_SIZES = [300, 700, 1000]
 export const AUTHOR = "Alan North"
+export const EMAIL = "alanscodelog@gmail.com"
 export const COPYRIGHT = ""
 export const COPYRIGHT_YEAR = 2022
 export const HEADER_LINKS = [
 	// {title: "Portfolio", href:"/portfolio"},
+	{ title: "Contact Me", href: `mailto:${EMAIL}` },
 	{ title: "Twitter", href: "https://twitter.com/alanscodelog" },
 	{ title: "Github", href: "https://github.com/alanscodelog" },
 	{ title: "Art", href: "https://alansartlog.com" },
 ]
 export const POSTS_PER_PAGE = 10
+export const GOOGLE_SITE_VERIFICATION = "xRBd80oMtEi_5B91Gcy6k2m4NhJdlx0WSkWIKD5yh1w"
+export const KEYWORDS = ["coding", "programming", "code", "program"]
