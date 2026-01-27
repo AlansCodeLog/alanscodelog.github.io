a593bb99a041ce98f894c854282af4927edaf991
Alan North <alanscodelog@gmail.com>
2026-01-26 21:57:21 -0300

    feat: added emphasis markdown component

---
+++ app/components/MdEmphasisBlock.vue
@@ -0,0 +1,15 @@
+<template>
+	<div
+		class="
+		md-emphasis-block
+		block
+		bg-primary-500/20
+		border-x-[calc(var(--spacing)*2)]
+		border-primary-500/30
+		p-4
+		-mx-6
+	"
+	>
+		<slot />
+	</div>
+</template>
